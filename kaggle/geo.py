442893b71eaaef1efab1390579cd5d8e3263c9b0
moleseaau <56633631+moleseaau@users.noreply.github.com>
2020-12-07 21:00:10 +0100

    .

--- kaggle/geo.py
+++ kaggle/geo.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import geopandas as gpd
 import plotly.express as px
+import plotly.graph_objects as go
 import plotly.offline
+
 
 happydata = pd.read_csv('2019.csv')
 happydata = happydata.set_index('Overall rank')
@@ -10,6 +12,24 @@ countries = gpd.read_file('countries.geojson')
 countries = countries[countries['ADMIN'] != 'Antarctica']
 countries = countries.rename(columns={'ADMIN': 'Country or region'})
 countries.to_crs(epsg=3857, inplace=True)
+
+def spatial2():
+    fig = go.Figure(data=go.Choropleth(
+        locations=happydata['Country or region'],
+        z=happydata['Score'].astype(float),
+        locationmode='country names',
+        colorscale='Viridis',
+        colorbar_title='Happiness<br>Score'
+    ))
+
+    fig.update_layout(
+        title_text='Happy'
+    )
+
+    fig.show()
+
+
+spatial2()
 
 
 def spatial():
@@ -43,4 +63,4 @@ def spatial():
     fig = dict(data=data, layout=layout)
     plotly.offline.plot(fig,validate=False,filename = 'test.html')
 
-spatial()
+#spatial()
